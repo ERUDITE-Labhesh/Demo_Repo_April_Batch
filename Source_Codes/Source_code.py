ccb7f016a93cefe0c9bc589ba83c9daf449f2633
ERUDITE-Labhesh <labhesh.l@consultadd.com>
2022-04-20 14:38:38 -0400

    First Commit, Adding folder to my remote repo

---
+++ Source_Codes/Source_code.py
@@ -0,0 +1,12 @@
+name = "Alex"
+
+def function_1(name):
+    print(f"Hello {name} Welcome To Training !! ")
+
+function_1(name)
+
+def function_2(name):
+    print("Hello This Function 2")
+
+
+    
